78edff67af04745a181485474f79e3f0e97f7761
Brenden-Therrien <therrienbrenden11@gmail.com>
2025-10-19 19:56:40 -0400

    fixed wordle bug again

--- SimpleWordle.py
+++ SimpleWordle.py
@@ -4,8 +4,7 @@ import string
 
 # All needed word info
 word = "crane"
-letters_in_word = list(word)
-word_length = len(letters_in_word)
+word_length = len(word)
 
 # print word as '_'s
 display = ['_'] * word_length
@@ -15,25 +14,41 @@ result = ""
 
 won = False
 
-trys = 5
+tries = 5
 
-while trys > 0 and won == False:
+while tries > 0 and won == False:
     print(' '.join(display))
-    guess = list(input("Please guess a word: ")) # Gets users word, and changes it to a list of arrays
+    # Gets users word, and changes it to a list of arrays
+    guess = input("Please guess a word: ")
+    letters_in_guess = list(guess)
+    results = ['' for _ in word]
+    letters_in_word = list(word)
 
-    for i in range(len(guess)):
-        if guess[i] == letters_in_word[i]:
-            result += f'\033[92m{guess[i]}\033[0m'
-        elif guess[i] in letters_in_word:
-            result += f'\033[93m{guess[i]}\033[0m'
-        else:
-            result += f'\033[91m{guess[i]}\033[0m'
-    trys -= 1
+    for i in range(len(letters_in_guess)):
+        if letters_in_guess[i] == letters_in_word[i]:
+            results[i] = f'\033[92m{letters_in_guess[i]}\033[0m'
+            letters_in_word[i] = " "
+            letters_in_guess[i] = " "
+
+    for i in range(len(letters_in_guess)):
+        if letters_in_guess[i] != " ":
+            if letters_in_guess[i] in letters_in_word:
+                results[i] = f'\033[93m{letters_in_guess[i]}\033[0m'
+                j = letters_in_word.index(letters_in_guess[i])
+                letters_in_word[j] = " "
+                letters_in_guess[i] = " "
+
+    for i in range(len(letters_in_guess)):
+        if letters_in_guess[i] != " ":
+            results[i] = f'\033[91m{letters_in_guess[i]}\033[0m'
+            
+    result = "".join(results)        
+    tries -= 1
     print(result)
     result = ""
-    if guess == letters_in_word:
+    if guess == word:
         won = True
         if won == True:
             print("You won!")
-    elif trys == 0:
+    elif tries == 0:
         print("Out of tries")
